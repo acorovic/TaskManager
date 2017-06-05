424435a9cbba12aba3560d701a27391fc5de9cba
student <rondor_1@uns.ac.rs>
2017-06-05 11:43:27 +0200

    added .so

--- TaskManager/statisticLib/jni/statisticCalculate.c
+++ TaskManager/statisticLib/jni/statisticCalculate.c
@@ -4,10 +4,11 @@
 #include <math.h>
 
 JNIEXPORT jint JNICALL Java_rtrtk_pnrs1_ra54_12014_CalculateStatisticsNative_getStatistic
-  (JNIEnv *env, jobject obj, jint totalCnt, jint finishedCnt);
+  (JNIEnv *env, jobject obj, jint totalCnt, jint finishedCnt)
 {
-	if(totalCntHigh != 0) {
-		return (jint) (finishedCnt*100/totalCnt);}
+	if(totalCnt != 0) {
+		return (jint) (finishedCnt*100/totalCnt);
+		}
     else {
         return (jint) 0;
     }
---
+++ TaskManager/statisticLib/obj/local/x86/objs/StatisticNative/statisticCalculate.o.d
@@ -0,0 +1,5 @@
+C:/Users/student.DOMAIN/Desktop/pnrs1/TaskManager/statisticLib/obj/local/x86/objs/StatisticNative/statisticCalculate.o: \
+  C:/Users/student.DOMAIN/Desktop/pnrs1/TaskManager/statisticLib/jni/statisticCalculate.c \
+  C:/Users/student.DOMAIN/Desktop/pnrs1/TaskManager/statisticLib/jni/statisticCalculate.h
+
+C:/Users/student.DOMAIN/Desktop/pnrs1/TaskManager/statisticLib/jni/statisticCalculate.h:
